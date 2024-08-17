486c6228d7bdec0bc92a44b60257233f8214dfb6
Stephen Brooks <stephenbrooks714@gmail.com>
2024-08-17 10:30:34 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/animations.scss";
+import "./styles/styles.scss";
 import { AuthProvider } from '@descope/react-sdk';
 import {
     BrowserRouter,
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,21 @@ import Features from "../components/Features.jsx"
 export default function HomePage() {
     return (
         <>
-            <main className={"container"}>
-                <section className={"row justify-content-center align-items-center"} style={{marginTop: "140px"}}>
-                    <div className={"col-lg-10 mb-5 pb-2 text-center"}>
-                        <p className={"lead mb-1"}>
-                            Hi, and welcome to
-                        </p>
-                        <h1 className={"display-4 fw-bold text-info"}>Your Passion 1st</h1>
-                    </div>
-                    <Features />
+            <div className={"home-intro"}>
+                <div className={"container"}>
+                    <section className={"row justify-content-center align-items-center"} style={{paddingTop: "140px"}}>
+                        <div className={"col-lg-8 mb-5 pb-2 text-center"}>
+                            <p className={"h4 fw-light mb-1"}>
+                                Let's help under resourced young adults overcome adversity and become the future innovators of the World!
+                            </p>
+                            <h1 className={"display-2 fw-bold text-info"}>Inspire & Be Inspired</h1>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <main className={"container my-5 py-5"}>
+                <section className={"row"}>
+                    <Features/>
                 </section>
             </main>
         </>
